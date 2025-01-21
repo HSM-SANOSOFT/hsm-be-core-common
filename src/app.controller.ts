36d8da28317b596543235d88874ae48dd95c78a1
Raul Santamaria <raulsantamariao@hotmail.com>
2025-01-21 18:19:34 +0000

    feat: add DTOs for parroquias, promociones, and validarCedula; update AppController with new message patterns

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,14 +1,37 @@
 import { Controller } from '@nestjs/common';
-import { MessagePattern } from '@nestjs/microservices';
+import { MessagePattern, Payload } from '@nestjs/microservices';
 
 import { AppService } from './app.service';
+import { ParroquiasDto } from './dto/parroquias.dto';
+import { PromocionesDto } from './dto/promociones.dto';
+import { ValidaCedulaDto } from './dto/validacedula.dto';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @MessagePattern('init')
+  @MessagePattern('initMS')
   InitMS() {
     return this.appService.InitMS();
+  }
+
+  @MessagePattern('combobox')
+  combobox() {
+    return this.appService.combobox();
+  }
+
+  @MessagePattern('promociones')
+  promociones(@Payload() promocionesDto: PromocionesDto) {
+    return this.appService.promociones(promocionesDto);
+  }
+
+  @MessagePattern('parroquias')
+  parroquias(@Payload() parroquiasDto: ParroquiasDto) {
+    return this.appService.parroquias(parroquiasDto);
+  }
+
+  @MessagePattern('validarCedula')
+  validarCedula(@Payload() validacedulaDto: ValidaCedulaDto) {
+    return this.appService.validarCedula(validacedulaDto);
   }
 }
